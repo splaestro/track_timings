515b9f6a1695809848f7f44b3f1e703a69c28f6f
Brian Young <splaestro@gmail.com>
2022-01-12 21:44:14 -0500

    all tests pass w/ decent test coverage

--- test/timing_test.rb
+++ test/timing_test.rb
@@ -4,7 +4,7 @@ require "track_timings"
 class TimingTest < Minitest::Test
 
     def setup
-        @tyming = TrackTimings::Timing.new(300)
+        @tyming = TrackTimings::Timing.new(0, 5 ,0)
         $stdout.puts name
     end
 
@@ -37,17 +37,24 @@ class TimingTest < Minitest::Test
         assert_equal(22, parse_tyming3.sec)
         assert_equal(5302, parse_tyming3.total_seconds)
 
-        #assert_raises(ArgumentError) {TrackTimings::Timing.parse("AA:BB.CCCC")}
         assert_raises(ArgumentError) {TrackTimings::Timing.parse("foo")}
+        assert_raises(ArgumentError) {TrackTimings::Timing.parse("AA:BB.CCCC")}
+        # note: leading zeroes are required!!
+        assert_raises(ArgumentError) {TrackTimings::Timing.parse("5:24")}
     end
     
     def test_to_s
         assert_equal("00:05:00", @tyming.to_s)
     end
 
-    def test_add
-        skip
-        assert_equal()
+    def test_at
+        at_tyming = TrackTimings::Timing.at(5000)
+        assert_instance_of(TrackTimings::Timing, at_tyming)
+        assert_kind_of(Time, at_tyming)
+        assert_equal(1, at_tyming.hour)
+        assert_equal(23, at_tyming.min)
+        assert_equal(20, at_tyming.sec)
+        assert_equal(5000, at_tyming.total_seconds)
     end
 
 end

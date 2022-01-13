638aee52bbe41497c47183122503bc8d93877f55
Brian Young <splaestro@gmail.com>
2022-01-12 21:34:56 -0500

    refactored all methods to work more similarly to methods in Time

--- lib/track_timings/timing.rb
+++ lib/track_timings/timing.rb
@@ -13,29 +13,30 @@ module TrackTimings
 
         attr_reader :total_seconds
 
-        def initialize(seconds)
-            @total_seconds = seconds
-            temp_time = Timing.at(@total_seconds)
-            super(1970, 1, 1, temp_time.hour, temp_time.min, temp_time.sec, "+00:00")
-            #super(BASE_TIME.year, BASE_TIME.month, BASE_TIME.day)
+        # both ::at and ::parse should call this function
+        def initialize(hours, minutes, seconds)
+            @total_seconds = (hours * 3600) + (minutes * 60) + seconds
+            super(1970, 1, 1, hours, minutes, seconds, "+00:00")
         end
         
         def Timing.at(seconds)
-            super(seconds, in:"+00:00")
+            hours = seconds/3600
+            minutes = (seconds%3600)/60
+            seconds = (seconds%3600)%60
+            Timing.new(hours, minutes, seconds)
+            # super(seconds, millisecs, :millisecond, in:"+00:00")
         end
-        
+
         def Timing.parse(time_string)
-            #TODO: also allow fractions of seconds--at present, any fractions will be discarded
-            time_parts = time_string.split(/\:/)
-            case time_parts.length
-                when 2
-                    hours, mins, secs = 0, time_parts[0].to_i, time_parts[1].to_i
-                when 3
-                    hours, mins, secs = time_parts[0].to_i, time_parts[1].to_i, time_parts[2].to_i
-                else
-                    raise ArgumentError, "Timing.parse expects a time spec like 'hh:MM:SS'"
+            #                   012         34        5      6 7 
+            parser = Regexp.new(/((\d{2}):)*((\d{2}):)(\d{2})(.(\d+))*/)
+            unless (parse_match = parser.match(time_string)) then
+                raise ArgumentError, "Timing.parse expects a time spec like 'hh:MM:SS.nnn'"
             end
-            Timing.new((hours * 3600) + (mins * 60) + secs)
+            hours, mins, secs = parse_match[2].to_i, parse_match[4].to_i, parse_match[5].to_i
+            # millisecs = parse_match[7].to_i
+            #TODO: also allow fractions of seconds--at present, any fractions are discarded
+            Timing.new(hours, mins, secs)
         end
         
         def to_s
